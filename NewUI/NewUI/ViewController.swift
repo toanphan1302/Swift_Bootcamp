062c92aee57dcdb8127c61f39b39a9e861ed7991
toanphan1302 <68579401+toanphan1302@users.noreply.github.com>
2021-08-18 22:11:54 -0700

    Add files via upload

---
+++ NewUI/NewUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewUI
+//
+//  Created by Toan on 8/18/21.
+//  Copyright © 2021 Toan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
